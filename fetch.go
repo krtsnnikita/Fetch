6cdda34b57e170951de48a0527b4ed8b5c926467
krtsnnikita <nkrtsn@gmail.com>
2022-03-31 11:42:02 +0300

    modifed fetch.go

--- fetch.go
+++ fetch.go
@@ -10,26 +10,26 @@ import (
 )
 
 func main() {
-        for _, url := range os.Args[1:] {
-				prefix := strings.Contains(url, "http://")
-				fmt.Println(prefix)
-				if !prefix {
-				url = "http://" + url
-			}
-                resp, err := http.Get(url)
-                if err != nil {
-                        fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-                        os.Exit(1)
-                }
+	for _, url := range os.Args[1:] {
+		prefix := strings.Contains(url, "http://")
+		fmt.Println(prefix)
+		if !prefix {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
 
-                b, err := io.Copy(os.Stdout, resp.Body)
-                resp.Body.Close()
-                if err != nil {
-                        fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
-                        os.Exit(1)
-                }
-                //fmt.Println(strings.Contains(url, "http://"))
-                fmt.Println(b)
-        }
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		//fmt.Println(strings.Contains(url, "http://"))
+		fmt.Println(b)
+	}
 
 }
